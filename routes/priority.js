5a74a17557cd3d9c444217a58142f0122e8d8981
Mark Lawrence <marklawrencecatubay@gmail.com>
2025-09-10 19:11:26 +0800

    Added routes to priority controller

---
+++ routes/priority.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = express.Router()
+const priorityController = require('../controllers/priorityController')
+
+router.post('/', priorityController.createPriority)
+router.get('/', priorityController.getAllPriorities)
+
+router.put('/:id', priorityController.updatePriority)
+router.delete('/:id', priorityController.deletePriority)
+
+module.exports = router
