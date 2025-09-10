3331501e138efdee7f3d1e8e4509ab32e75d8a1f
Mark Lawrence <marklawrencecatubay@gmail.com>
2025-09-10 19:12:44 +0800

    Mounted the routes to the express app

--- app.js
+++ app.js
@@ -5,19 +5,20 @@ const { db, initDB } = require('./database')
 const PORT = 3000
 const app = express()
 const taskRoutes = require('./routes/tasks')
+const priorityRoutes = require('./routes/priority')
 
 // middlewares
 app.use(express.json())
 app.use(cors())
 app.use(express.static('public'))
 app.use('/api/tasks', taskRoutes)
+app.use('/api/priority', priorityRoutes)
 
 initDB()
 
 app.listen(PORT, () => {
     console.log(`The port is listening at http://localhost:${PORT} ...`)
 })
-
 
 app.get('/', (req, res) => {
     res.status(200).json({success:true,data:"Welcome to task-manager app!"})
