d4c4e27177b3e5fcb39091361e9ffe0d83fab99f
Mark Lawrence <marklawrencecatubay@gmail.com>
2025-09-13 14:18:49 +0800

    Removed (,) causing SQL Error

--- controllers/priorityController.js
+++ controllers/priorityController.js
@@ -43,7 +43,7 @@ const updatePriority = (req, res) => {
     const query = `
         UPDATE tasks
         SET
-            name = COALESCE(?, name),
+            name = COALESCE(?, name)
         WHERE id = ?
     `
     const params = [name, id]
